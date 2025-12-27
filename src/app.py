cc9fa2f1e22c192266d35d5a1e39227c721868eb
Heitor Galvão <heitorjunk@gmail.com>
2025-12-26 21:17:03 -0300

    <feat>: surpresa... (1.5/2)

--- src/app.py
+++ src/app.py
@@ -22,12 +22,22 @@ def content():
 def newyear():
     return render_template('pages/newyear.html')
 
-
 @app.route('/page1')
 def page1():
     return render_template('pages/page1.html')
 
-
 @app.route('/page2')
 def page2():
     return render_template('pages/page2.html')
+
+@app.route('/page3')
+def page3():
+    return render_template('pages/page3.html')
+
+@app.route('/page4')
+def page4():
+    return render_template('pages/page4.html')
+
+@app.route('/page5')
+def page5():
+    return render_template('pages/page5.html')
